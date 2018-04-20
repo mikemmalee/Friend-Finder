ce17282954c87d8e17aa786f818701286ef76fce
mikeemmalee <mmikova91@gmail.com>
2018-04-19 20:33:33 -0700

    Added home page and survey HTML

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -26,3 +26,7 @@
 // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 
 // The modal should display both the name and picture of the closest match.
+
+var path = require('path');
+
+var friendData = require('../data/friends.js');
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+app.use(express.static("public"));
+
+//App routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});

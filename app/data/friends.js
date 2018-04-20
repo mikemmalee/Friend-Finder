20500524bbd72f64feef35a66f987f03a6a8257e
mikeemmalee <mmikova91@gmail.com>
2018-04-19 21:20:39 -0700

    Added data to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,102 @@
 
 var friendsArray = [
     {
-        name: "Ahmed",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Monica",
+        photo:"https://ecelebrityfacts.com/images/1148/courteney-cox-1498459042.jpg",
         scores:[
-            5,
-            1,
+            3,
             4,
-            4,
-            5,
-            1,
             2,
+            3,
+            3,
+            3,
             5,
-            4,
-            1
+            3,
+            3,
+            4
         ]
     },
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Joey",
+        photo: "https://pbs.twimg.com/media/CgcqErlUUAAQ07A.jpg",
+        scores:[
+            5,
+            2,
+            4,
+            2,
+            3,
+            1,
+            4,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Phoebe",
+        photo: "https://i.pinimg.com/736x/5b/ef/82/5bef82986de31e70eb2dafcbfa9ccdd5--phoebe-buffay-icons.jpg",
         scores:[
             5,
             1,
+            5,
+            2,
+            4,
+            3,
+            4,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Chandler",
+        photo: "https://pbs.twimg.com/profile_images/917789882776522752/7nZeyOPc_400x400.jpg",
+        scores:[
+            3,
+            5,
+            4,
+            3,
+            5,
+            2,
+            4,
+            2,
+            4,
+            2
+        ]
+    },
+    {
+        name: "Rachel",
+        photo: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fhellogiggles.com%2Fwp-content%2Fuploads%2F2016%2F01%2F07%2FFriends-Rachel-Green-Kitchen.jpg&w=700&q=85",
+        scores:[
+            3,
+            3,
+            4,
+            2,
+            4,
+            1,
+            5,
             4,
             4,
+            3
+        ]
+    },
+    {
+        name: "Ross",
+        photo: "https://www.telegraph.co.uk/content/dam/films/2017/07/07/mjio9545-1383063845-7_trans_NvBQzQNjv4BqYO0p0mIshyPeegbe8-9U_Xl6rCiFLrn669kcdzcPpE0.jpg?imwidth=450",
+        scores:[
+            5,
+            5,
             5,
             1,
             2,
+            3,
             5,
             4,
-            1
+            3,
+            3
         ]
     }
-  ];
+];
   
-// Note how we export the array. This makes it accessible to other files using require.
+
 module.exports = friendsArray;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.text());
 
 app.use(express.static("public"));
 
-//App routes
+// App routes
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
